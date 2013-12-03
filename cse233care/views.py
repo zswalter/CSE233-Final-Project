49210ccccd63968f8ecf542804d42320ddab33e2
Nicholas Land <ndland@oakland.edu>
2013-12-03 11:14:06 -0500

    Created a new view. It's plain, but it'll get the ball rolling

--- cse233care/views.py
+++ cse233care/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
 
-# Create your views here.
+def mainview(request):
+    return HttpResponse("""
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        Welcome to CSE233Care!
+    </title>
+</head>
+<body>
+    <h1>Here is the main page</h1>
+    <p>Here is where we'll have some bootstrappy things</p>
+</body>
+</html>
+    """)
--- final_project/urls.py
+++ final_project/urls.py
@@ -1,12 +1,8 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
-admin.autodiscover()
+# from django.contrib import admin
+from cse233care import views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'final_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url('^$', views.mainview, name='index'),
 )
