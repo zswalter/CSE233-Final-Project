35d4b290929c2a18234ed24c2c3b5bf49ef6b06d
Jacob Pleiness <jacobpleiness@jacobpleiness.com>
2013-12-03 21:44:50 -0500

    Fixed indentation and spacing

--- cse233care/views.py
+++ cse233care/views.py
@@ -1,17 +1,19 @@
 from django.http.response import HttpResponse
 
+
 def mainview(request):
     return HttpResponse("""
-<!DOCTYPE html>
-<html>
-<head>
-    <title>
-        Welcome to CSE233Care!
-    </title>
-</head>
-<body>
-    <h1>Here is the main page</h1>
-    <p>Here is where we'll have some bootstrappy things</p>
-</body>
-</html>
-    """)
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>
+            Welcome to CSE233Care!
+        </title>
+    </head>
+    <body>
+        <h1>Here is the main page</h1>
+        <p>Here is where we'll have some bootstrappy things</p>
+    </body>
+    </html>
+        """)
+
