663a64ab78aabf1a44420cd577c3769e29946c81
Zachary Walters <zachary0191@gmail.com>
2013-12-05 22:05:04 -0500

    Test CSS, Success!!! CSS is now working!!!
    Though it needs lots of work to be completed!

--- final_project/settings.py
+++ final_project/settings.py
@@ -81,6 +81,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static')
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'staticfiles'),
+)
 
 TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(__file__), 'templates'),
--- final_project/urls.py
+++ final_project/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # from django.contrib import admin
 from cse233care import views
